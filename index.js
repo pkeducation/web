8a8cd3b5876be8ac7ca7e399193b637a80694bb5
gulgulia <gajendra.gulgulia@tttech-auto.com>
2021-01-28 17:00:24 +0100

    PKW-060: fixed animaiton, mobile for features

--- index.js
+++ index.js
@@ -10,14 +10,10 @@ function detectMob() {
 
 $(window).scroll(function() {
   if ($(this).scrollTop() > 275) {
-      $('.to-left').stop().animate({ right: '-60%' }, 1000);
+      $('.to-left').stop().animate({ left: '60%' }, 1000);
+      $('.to-right').stop().animate({ left: '5%' },1000);
   } 
 });
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 275) {
-        $('.to-right').stop().animate({ left: '2%' },1000);
-    } 
-}); 
 
 
 
