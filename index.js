f2d1ffa01e5a343bbc9e0d869dc0d507a8f94a43
anokhi <anokhias@gmail.com>
2021-01-28 20:05:50 +0100

    PKW-070 Icons and arrows adjusted

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function detectMob() {
 $(window).scroll(function() {
   if ($(this).scrollTop() > 275) {
       $('.to-left').stop().animate({ left: '60%' }, 1000);
-      $('.to-right').stop().animate({ left: '5%' },1000);
+      $('.to-right').stop().animate({ left: '2%' },1000);
   } 
 });
 
