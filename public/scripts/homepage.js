eced3d1ab482a41b15c4fa66daa8356d821ba849
gulgulia <gajendra.gulgulia@tttech-auto.com>
2021-02-23 22:00:01 +0100

    PKW-003: pricing and why us link working

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -6,15 +6,12 @@ function detectMob() {
     }
  }
 
-
 $(window).scroll(function() {
   if ($(this).scrollTop() > 275) {
       $('.to-left').stop().animate({ left: '60%' }, 1000);
       $('.to-right').stop().animate({ left: '2%' },1000);
   } 
 });
-
-
 
 
 $(".pricing-link").click(function() {
