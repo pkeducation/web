6819aaab778fc18c43bbb20f521ca539829b8aca
gulgulia <gajendra.gulgulia@tttech-auto.com>
2021-02-23 21:37:30 +0100

    PKW-001-nodejs migration 1: javascript with hompage functionalities not working

--- index.js
+++ index.js
@@ -1,58 +1,19 @@
 
-function detectMob() {
-    if($(window).width() <= 992) {
-      return true;
-    } else {
-      return false;
-    }
- }
-
-
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 275) {
-      $('.to-left').stop().animate({ left: '60%' }, 1000);
-      $('.to-right').stop().animate({ left: '2%' },1000);
-  } 
-});
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
 
 
 
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-$(".pricing-link").click(function() {
-  $('html, body').animate({
-      scrollTop: $("#pricing").offset().top
-  }, 2000);
-});
-
-var featuresSection = "#features";
-if(detectMob()){
-  featuresSection +="2";
-}
-$(".whyUs-link").click(function() {
-  $('html, body').animate({
-      scrollTop: $(featuresSection).offset().top
-  }, 2000);
-});
-
-$(".contactUs-link").click(function() {
-  $('html, body').animate({
-      scrollTop: $("#contactUs").offset().top
-  }, 2000);
-});
-
-/* adding back to top button */
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-var mybutton = document.getElementById("toTopBtn");
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-$("#toTopBtn").on('click', function(){
-  $("html, body").animate({scrollTop: $("#top").offset().top}, 500);
+ app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
 })
+
+app.listen(process.env.PORT ||3000, function(){
+  console.log("pk education app started");
+});
