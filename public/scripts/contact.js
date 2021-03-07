3367e230d37fef2929292970aa4a08fcbc0e5894
gulgulia <gajendra.gulgulia@gmail.com>
2021-03-07 23:50:53 +0100

    PKW-029: migrate common js to common.js

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -1,33 +1,12 @@
-/* top button functionality */
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
-  
-  var mybutton = document.getElementById("toTopBtn");
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function() {scrollFunction()};
-  
-  $("#toTopBtn").on('click', function(){
-    $("html, body").animate({scrollTop: $("#top").offset().top}, 500);
-  })
-
   /* form functionality */
   $(function () {
-
     // init the validator
     // validator files are included in the download package
     // otherwise download from http://1000hz.github.io/bootstrap-validator
 
     $('#contact-form').validator();
-
-
     // when the form is submitted
     $('#contact-form').on('submit', function (e) {
-
         // if the validator does not prevent form submit
         if (!e.isDefaultPrevented()) {
             var url = "contact.php";
