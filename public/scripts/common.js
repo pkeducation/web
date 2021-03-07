9c9b73e2e3fd961437500affb61b8fe73551b9e5
gulgulia <gajendra.gulgulia@gmail.com>
2021-03-07 23:57:30 +0100

    PKW-029-2: added common.js

---
+++ public/scripts/common.js
@@ -0,0 +1,24 @@
+function detectMob() {
+    if($(window).width() <= 992) {
+      return true;
+    } else {
+      return false;
+    }
+ }
+
+/* adding back to top button */
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  var mybutton = document.getElementById("toTopBtn");
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  $("#toTopBtn").on('click', function(){
+    $("html, body").animate({scrollTop: $("#top").offset().top}, 500);
+  })
