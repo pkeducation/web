a26f38f8edc5a6f39d4d8905395fdebcaaf8aa6d
anokhi <anokhias@gmail.com>
2021-03-16 20:33:56 +0100

    PKW-036: reset func for form

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -4,15 +4,18 @@ function sendMail(){
     event.preventDefault();
     console.log("from send mail func");
     var tempParams = {
-        name: document.getElementById("FName").value,
-        last_name: document.getElementById("LName").value,
-        email: document.getElementById("SEmail").value
+        studentName: document.getElementById("FName").value,
+        studentPhone: document.getElementById("SPhone").value,
+        sutdentEmail: document.getElementById("SEmail").value,
+        parentName: document.getElementById("PName").value,
+        parentPhone: document.getElementById("PPhone").value,
+        Subject: document.getElementById("SSubject").value,
+        Message: document.getElementById("Message").value
     };
-
-    console.log("created tempParams");
-    emailjs.send('service_wwrp9hk', 'template_or7i0i9', tempParams)
+     emailjs.send('service_wwrp9hk', 'template_or7i0i9', tempParams)
     .then(function(res){
         console.log("success", res.status);
+        document.getElementById("contact-form").reset();
     });
 }
 
